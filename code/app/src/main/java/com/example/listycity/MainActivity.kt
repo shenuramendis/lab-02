6b03a925c25972b3eb9f2b587a7b9ac837f44f72
shenura <shenura.mendis@gmail.com>
2026-09-10 17:06:13 -0600

    clickable buttons and shit but no delete

--- code/app/src/main/java/com/example/listycity/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +14,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -103,13 +107,14 @@ fun CityListScreen(
 
 @Composable
 fun CityRow(city: String) {
-    Text (
-        text = city,
-        fontSize = 28.sp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 18.dp, vertical = 14.dp)
-    )
+    var buttonSelected by remember {mutableStateOf (value = false)}
+    Button (
+        onClick = { buttonSelected = !buttonSelected },
+        colors = ButtonDefaults.buttonColors(containerColor = if (buttonSelected) Color.Black else Color.Blue),
+        enabled = true,
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 18.dp, vertical = 8.dp),
+        ){ Text(text = city, fontSize = 28.sp) }
+
 }
 
 class CityRepository {
